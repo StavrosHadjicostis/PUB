4303a99f593b8ca24f2061a39e7b6ff18e721932
Stavros Hadjicostis <106922068+StavrosHadjicostis@users.noreply.github.com>
2023-02-26 17:43:36 +0200

    Add files via upload
    added the new html files

---
+++ pub.js
@@ -0,0 +1,19 @@
+$("#reserv").click(function() {
+
+  location.href ="reservation.html";
+});
+
+$("#merch").click(function() {
+
+  location.href ="merchandise.html";
+});
+
+$("#event").click(function() {
+
+  location.href ="events.html";
+});
+
+$("#career").click(function() {
+
+  location.href ="careers.html";
+});
